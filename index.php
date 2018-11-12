4ed6652ce3d328db06980f369758a523009ed911
MacbookPro <macbookpro@MacbookPros-MacBook-Pro.local>
2018-11-11 20:55:54 -0600

    Navbar

--- index.php
+++ index.php
@@ -1,13 +1,39 @@
-<<!DOCTYPE html>
+<!DOCTYPE html>
 <html>
 <head>
     <meta charset="utf-8" />
     <title>QUE SED</title>
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-    <link rel="stylesheet" type="text/css" media="screen" href="main.css" />
-    <script src="main.js"></script>
+    <meta name="viewport" content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">    <link rel="stylesheet" type="text/css" media="screen" href="main.css" />
+    <link rel="stylesheet" href="css/fonts.css">
+    <link rel="stylesheet" href="css/styles.css">
+    <script src="http://code.jquery.com/jquery-latest.js"></script>
+    <script src="js/effect_nav.js"></script>
 </head>
 <body>
-    
+    <header>
+        <div class="menu_bar">
+            <a href="#" class="btn-menu"><span class="icon-menu"></span>Menu</a>
+        </div>
+        <nav>
+            <ul>
+                <li><a href="#"><span class="icon-home"></span> Home</a></li>
+                <li><a href="#"><span class="icon-bubble"></span> About</a></li>
+                <li><a href="#"><span class="icon-cog"></span> Logout</a></li>
+            </ul>
+        </nav>
+        
+        
+
+    </header>
+
+    <section>
+            <p>
+                <span>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Earum praesentium dolorem ducimus ipsa totam necessitatibus culpa distinctio quis mollitia et fugit, atque, odit perspiciatis saepe optio doloribus voluptas consequatur unde.</span>
+                <span>Soluta corrupti qui nisi, dignissimos iusto reprehenderit sequi placeat quasi minima. In, architecto eveniet! Nisi nulla exercitationem nemo magnam provident velit ex hic amet. Similique sint magni nesciunt rem perspiciatis!</span>
+                <span>Sint dolore unde est repudiandae rerum quasi nihil cupiditate. Voluptate qui dolor quasi laboriosam explicabo architecto, unde voluptatum eius perferendis sit blanditiis quod illum fugit. Alias reiciendis consectetur corrupti distinctio?</span>
+                <span>Perferendis ipsam eaque repellat fuga id, hic nobis inventore voluptates dolores laborum, at maxime neque obcaecati repudiandae quo! Quod adipisci saepe mollitia sequi fugit dolore, repellat quisquam autem accusamus dignissimos?</span>
+                <span>Amet debitis aspernatur harum ipsam fugit, hic aut cupiditate quos tempora accusamus nulla nostrum tenetur quo illo iusto sint sunt exercitationem! Officiis officia debitis in, quis dicta at vero facilis.</span>
+            </p>
+        </section>
 </body>
 </html>
