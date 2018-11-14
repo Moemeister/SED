851c24838ffb38e6b676b28ff2aa63a6929eb5ea
MacbookPro <macbookpro@MacbookPros-MacBook-Pro.local>
2018-11-13 21:42:23 -0600

    CSS for login

--- core/conection.php
+++ core/conection.php
@@ -3,7 +3,7 @@
     if ($mysqli->connect_errno) {
         echo "Fallo al conectar a MySQL: (" . $mysqli->connect_errno . ") " . $mysqli->connect_error;
     }
-    echo $mysqli->host_info . "\n";
+    //echo $mysqli->host_info . "\n";
     /*$link =  'mysql:host=127.0.0.1;dbname=Shop';
     $user = 'root';
     $pass = '';
--- register.php
+++ register.php
@@ -1,28 +1,27 @@
 <!DOCTYPE html>
-<?php
-    include "core/conection.php"
-?>
 <html>
 <head>
     <meta charset="utf-8" />
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <title>Login</title>
+    <title>Register</title>
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    <link rel="stylesheet" type="text/css" media="screen" href="css/style_login.css" />
+    <link rel="stylesheet" type="text/css" media="screen" href="main.css" />
     <script src="main.js"></script>
 </head>
 <body>
-    <div class="login-box">
-        <h1>Log In</h1>
-        <form action="check_login.php" method="POST">
-            <label for="user">Username</label>
+<div class="register-box">
+        <h1>Register</h1>
+        <form action="" method="POST">
+            <label for="name">User</label>
+            <input type="text" name="name" id="name" placeholder="Enter your name">
+            <label for="user">Pass</label>
             <input type="text" name="user" id="user" placeholder="Enter Username">
             <label for="pass">Pass</label>
             <input type="password" name="pass" id="pass" placeholder="Enter Password">
-            <input type="submit"name="login" value="Log In">
-            <a href="register.php">Don't have An account?</a>
+            <input type="submit"name="sign" value="Sign In">
             
         </form>
     </div>
+    
 </body>
 </html>
